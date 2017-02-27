86604a9a8ddb6ff01bb280b512ff18a9a277232d
Devanshu Jain <devjain@nlpgrid.seas.upenn.edu>
2017-02-27 17:17:27 -0500

    Results formatted

--- Experiments/Experiment-1/evaluate.sh
+++ Experiments/Experiment-1/evaluate.sh
@@ -2,9 +2,11 @@ echo "Will evaluate for top $1 transliterations"
 
 rm -r result
 mkdir result
-for i in $(seq 0 9)
+for j in $(seq 60 10 90)
 do
-    for j in $(seq 60 10 90)
+    echo "Avg Normalised Edit Distance, Accuracy, F-Score, MRR, MAP" >> result/train-$j
+    echo "---------------------------------------------------------" >> result/train-$j
+    for i in $(seq 0 9)
     do
         echo "rep-$i/train-$j"
         rm output
@@ -17,8 +19,26 @@ do
     done
 done
 
-#for i in $(seq 60 10 90)
-#do
-#    echo "train-$i"
-#    awk '{ total += $1; count++ } END { print total/count }' result/train-$i
-#done
+for i in $(seq 60 10 90)
+do
+    echo "train-$i"
+    awk '
+        BEGIN{
+            FS=", "; 
+            ORS=" " ; 
+            for(i=1;i<=NF;i++) 
+                total[i]=0; 
+            count = 0;
+        } 
+        NR>2{ 
+            for(i=1;i<=NF;i++) 
+                total[i] += $i; 
+            count ++;
+        } 
+        END{
+            print "---------------------------------------------------------\n"
+            for(i=0;i<=NF;i++)
+                print total[i]/count; 
+        }
+    ' result/train-$i >> result/train-$i
+done
